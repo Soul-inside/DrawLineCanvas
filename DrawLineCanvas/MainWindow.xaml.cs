1a16485dafe31adc153df21de8477af544a20aca
Soul-inside <soul_inside@mail.ru>
2014-11-21 15:31:28 +0300

    Область определения

--- DrawLineCanvas/MainWindow.xaml.cs
+++ DrawLineCanvas/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Net.Mime;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -67,10 +66,16 @@ namespace DrawLineCanvas
 		/// </summary>
 		private DateTime _time;
 
+		/// <summary>
+		/// Выделение области определения функции
+		/// </summary>
+		private bool _drowRectangle;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			ResizeImage();
+			_drowRectangle = false;
 		}
 
 		private void ResizeImage()
@@ -86,77 +91,95 @@ namespace DrawLineCanvas
 		/// <param name="e"></param>
 		private void CnvDraw_MouseMove(object sender, MouseEventArgs e)
 		{
-			// если левая кнопка нажата, рисуем временную линию
-			if (e.LeftButton == MouseButtonState.Pressed && (DateTime.Now - _time).TotalMilliseconds > 100)
+			//режим задания области определения
+			if (_drowRectangle)
 			{
-				// говорим флагу что мы начали рисовать линию
-				_drawingMode = true;
-				CnvDraw.Children.Remove(_tempLine);
-				// мы рисуем первую линию
-				if (_firstLine)
+				var rectangle = new Rectangle();
+				rectangle.Stroke = Brushes.Red;
+				rectangle.Width = 300;
+				rectangle.Height = 300;
+				Canvas.SetLeft(rectangle, 0);
+				Canvas.SetTop(rectangle, 0);
+				CnvDraw.Children.Add(rectangle);
+				//переопределяем канвас под область определения
+				CnvDraw.Height = rectangle.Height;
+				CnvDraw.Width = rectangle.Width;
+				_drowRectangle = false;
+			}
+			else
+			{
+				// если левая кнопка нажата, рисуем временную линию
+				if (e.LeftButton == MouseButtonState.Pressed && (DateTime.Now - _time).TotalMilliseconds > 100)
 				{
-					_prevX = e.GetPosition(CnvDraw).X;
-					_prevY = 0;
-					_newX = _prevX;
-					_newY = e.GetPosition(CnvDraw).Y;
-				}
-					// мы рисуем не первую линию
-				else
-				{
-					// рисуем вертикальную линию
-					if (_isVertical)
+					// говорим флагу что мы начали рисовать линию
+					_drawingMode = true;
+					CnvDraw.Children.Remove(_tempLine);
+					// мы рисуем первую линию
+					if (_firstLine)
 					{
+						_prevX = e.GetPosition(CnvDraw).X;
+						_prevY = 0;
 						_newX = _prevX;
 						_newY = e.GetPosition(CnvDraw).Y;
 					}
-						// рисуем горизонтальную линию
+					// мы рисуем не первую линию
 					else
 					{
-						_newX = e.GetPosition(CnvDraw).X;
-						_newY = _prevY;
+						// рисуем вертикальную линию
+						if (_isVertical)
+						{
+							_newX = _prevX;
+							_newY = e.GetPosition(CnvDraw).Y;
+						}
+						// рисуем горизонтальную линию
+						else
+						{
+							_newX = e.GetPosition(CnvDraw).X;
+							_newY = _prevY;
+						}
 					}
+					_tempLine = new Line
+					{
+						X1 = _prevX,
+						Y1 = _prevY,
+						X2 = _newX,
+						Y2 = _newY,
+						Stroke = Brushes.Red
+					};
+					CnvDraw.Children.Add(_tempLine);
 				}
-				_tempLine = new Line
-				{
-					X1 = _prevX,
-					Y1 = _prevY,
-					X2 = _newX,
-					Y2 = _newY,
-					Stroke = Brushes.Red
-				};
-				CnvDraw.Children.Add(_tempLine);
-			}
 				// если левая кнопка не нажата, и мы были в режиме рисовашек
-			else if (_drawingMode)
-			{
-				_drawingMode = false;
-				// Йухххууу! Мы нарисовали первую линию епта!
-				if (_firstLine)
+				else if (_drawingMode)
 				{
-					_firstLine = false;
+					_drawingMode = false;
+					// Йухххууу! Мы нарисовали первую линию епта!
+					if (_firstLine)
+					{
+						_firstLine = false;
+					}
+
+					// создаем новую линию взамен временной
+					var line = new Line
+					{
+						X1 = _prevX,
+						Y1 = _prevY,
+						X2 = _newX,
+						Y2 = _newY,
+						Stroke = Brushes.Black
+					};
+					CnvDraw.Children.Add(line);
+					CnvDraw.Children.Remove(_tempLine);
+					_linesList.Add(new LineInfo(_prevX, _prevY, line));
+					X.Content = "(" + line.X1 + " : " + line.Y1 + ")";
+					Y.Content = "(" + line.X2 + " : " + line.Y2 + ")";
+
+					// запоминаем координаты
+					_prevX = _newX;
+					_prevY = _newY;
+
+					// переключаем режим рисования на обратный
+					_isVertical = !_isVertical;
 				}
-
-				// создаем новую линию взамен временной
-				var line = new Line
-				{
-					X1 = _prevX,
-					Y1 = _prevY,
-					X2 = _newX,
-					Y2 = _newY,
-					Stroke = Brushes.Black
-				};
-				CnvDraw.Children.Add(line);
-				CnvDraw.Children.Remove(_tempLine);
-				_linesList.Add(new LineInfo(_prevX, _prevY, line));
-				X.Content = "(" + line.X1 + " : " + line.Y1 + ")";
-				Y.Content = "(" + line.X2 + " : " + line.Y2 + ")";
-
-				// запоминаем координаты
-				_prevX = _newX;
-				_prevY = _newY;
-
-				// переключаем режим рисования на обратный
-				_isVertical = !_isVertical;
 			}
 		}
 
@@ -219,7 +242,7 @@ namespace DrawLineCanvas
 		/// <param name="e"></param>
 		private void ButCoord_Click(object sender, System.Windows.RoutedEventArgs e)
 		{
-
+			_drowRectangle = true;
 		}
 
 		/// <summary>
